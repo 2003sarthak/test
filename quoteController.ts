3aef0373d7ace1ac860429d5d6726f4022c1b617
Sarthak Goyal <210106060@hbtu.ac.in>
2025-04-06 15:20:08 +0530

    Update quoteController.ts

--- quoteController.ts
+++ quoteController.ts
@@ -213,3 +213,130 @@ namespace CapstoneBackend.Models
 
     }
 }
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CapstoneBackend.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace CapstoneBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("allowCors")]
+    public class QuoteModelsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public QuoteModelsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/QuoteModels
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<QuoteModel>>> GetQuotes()
+        {
+            return await _context.Quotes.Include(q=>q.broker).ToListAsync();
+        }
+
+        // GET: api/QuoteModels/5
+        [HttpGet("get/{id}")]
+        public async Task<ActionResult<QuoteModel>> GetQuoteModel(int id)
+        {
+            var quoteModel = await _context.Quotes.FindAsync(id);
+
+            if (quoteModel == null)
+            {
+                return NotFound("Quote Not Found");
+            }
+
+            return quoteModel;
+        }
+
+
+        // PUT: api/QuoteModels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> PutQuoteModel(int id, QuoteModel quoteModel)
+        {
+            if (id != quoteModel.id)
+            {
+                return BadRequest("Quote ID Missmatch");
+            }
+
+            _context.Entry(quoteModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuoteModelExists(id))
+                {
+                    return NotFound("Quote Not Found");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(new { message = "Quote Edited successfully " });
+        }
+
+        // POST: api/QuoteModels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("create")]
+        public async Task<ActionResult<QuoteModel>> PostQuoteModel([FromBody] QuoteModel quoteModel, [FromHeader] string userEmail)
+        {
+            if(quoteModel == null)
+            {
+                return BadRequest("Invalid data");
+            }
+            var broker =await _context.Users.Include(u=>u.quotes).FirstOrDefaultAsync(u => u.email == userEmail);
+            if (broker == null)
+            {
+                return Unauthorized("Invalid user");
+            }
+            quoteModel.broker = null;
+            quoteModel.brokerId = broker.BrokerId;
+            quoteModel.brokerName = broker.fullName;
+            broker.quotes.Add(quoteModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuoteModel", new { id = quoteModel.id }, quoteModel);
+        }
+
+        // DELETE: api/QuoteModels/5
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteQuoteModel(int id)
+        {
+            var quoteModel = await _context.Quotes.FindAsync(id);
+            if (quoteModel == null)
+            {
+                return NotFound("Quote Not Found");
+            }
+
+            _context.Quotes.Remove(quoteModel);
+            await _context.SaveChangesAsync();
+
+            return Ok(new {message="Quote Deleted successfully "});
+        }
+
+
+
+        private bool QuoteModelExists(int id)
+        {
+            return _context.Quotes.Any(e => e.id == id);
+        }
+    }
+}
+
